8767b43fd8101bb13b48227003c35b6ea53e0b80
MarcosDoloko <92060803+MarcosDoloko@users.noreply.github.com>
2021-11-24 00:16:09 -0300

    ALGORITMOTRIX V 1.2.4 - Elaboração Teste01

---
+++ PBG-PI/src/Teste.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import 
+import java.util.Scanner;
+
+public class Teste {
+	
+	//teste no main principal
+	public static void main(String[] args) {
+
+		questao1();
+
+		  if (opcao.equals("a") || opcao.equals("b") || opcao.equals("c")
+				|| opcao.equals("d") || opcao.equals("e") || opcao.equals("A") || opcao.equals("B") || opcao.equals("C")
+				|| opcao.equals("D") || opcao.equals("E")) {
+			System.out.println("OK! Passou do teste.");
+		} else
+			System.out.println("Oops! Deu um resultado não esperado: " + opcao);
+	}
+}
